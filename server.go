df84686e1ce379eea59857b695db2d362a1cdf6f
Joe Radkowski <joe@radkowski.org>
2016-01-21 23:18:26 -0500

    Quick integration of GeoIP (free) database

--- server.go
+++ server.go
@@ -1,11 +1,31 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/oschwald/geoip2-golang"
+	"log"
+	"net"
 )
 
 func info(c *gin.Context) {
-	address := gin.H{"address": c.Request.RemoteAddr}
+	file := "GeoIP/GeoLite2-City.mmdb"
+
+	db, err := geoip2.Open(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	// ip := net.IP(c.Request.RemoteAddr)
+	ip := net.ParseIP("73.200.70.234")
+	fmt.Println(ip)
+	record, err := db.City(ip)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// address := gin.H{"ip": c.Request.RemoteAddr, "country": record.Country.Names, "location": record.Location, "timezone": record.Location.TimeZone}
+	address := gin.H{"ip": ip, "details": record}
 	c.JSON(200, address)
 }
 
